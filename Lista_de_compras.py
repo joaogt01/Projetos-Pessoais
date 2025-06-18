f66da1a3471f0acf31afd7083b8a04a86c248ff1
joaogt01 <joao.dev06@gmail.com>
2025-06-17 23:54:20 -0300

    Update Lista_de_compras.py

--- Lista_de_compras.py
+++ Lista_de_compras.py
@@ -15,16 +15,33 @@ while laco == True:
         produto_add = input()
         print("Digite a quantidade a ser adicionada: ")
         quantidade_add = int(input())
-        registo_compras['produto'] = produto_add
-        registo_compras['quantidade'] = quantidade_add
+        registo_compras = {
+            'produto': produto_add,
+            'quantidade': quantidade_add
+        }
         lista_de_compras.append(registo_compras)
         continue
     elif escolha == 2:
-        print(lista_de_compras)
-        print("Escolha o produto a ser excluido da lista")
+        print("Lista atual:")
+        for i,item in enumerate(lista_de_compras):
+            print(f"{i+1}. {item['produto']} - {item['quantidade']}")
+        print("Digite o nome do produto a ser removido")
         produto_remove = input().lower()
-        if produto_remove in lista_de_compras:
-            lista_de_compras.remove(registo_compras[produto_remove])
-            print(lista_de_compras)
+
+        encontrado = False
+        for item in lista_de_compras:
+            if item['produto'].lower() == produto_remove:
+                lista_de_compras.remove(item)
+                print(f"{produto_remove} removido com sucesso!")
+                encontrado = True
+                break
+        if not encontrado:
+            print("Produto não encontrado na lista.")
     elif escolha == 3:
-        print(lista_de_compras)
+        for item in lista_de_compras:
+            print(f"- {item['produto']} (Quantidade: {item['quantidade']})")
+    elif escolha == 4:
+        laco = False
+        print("Encerrando")
+    else:
+        print("Opção invalida. Tente novamente")
